488151b29d95f53bc765fc6531082e21e706303b
Igor <igoremin@gmail.com>
2020-05-15 11:45:33 +0300

    Добавил настройки для работы с MySQL

--- site_engine/settings.py
+++ site_engine/settings.py
@@ -88,12 +88,23 @@ WSGI_APPLICATION = 'site_engine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'CONN_MAX_AGE': 3600,
+            'OPTIONS': {
+                'read_default_file': f'{BASE_DIR}/my.cnf'
+            }
+        }
+    }
 
 
 # Password validation
